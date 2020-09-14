d18e76e2bafc5e9053ff809a72f072307994181a
Tim Sorokin <timsorokin352@gmail.com>
2020-09-14 22:24:31 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members && Array.isArray(members)) {
+    let namesArray = [];
+    members.forEach((item) => {
+      if (typeof item == "string") {
+        let trimmedItem = item.trim();
+        namesArray.push(trimmedItem[0].toUpperCase());
+      }
+    });
+    return namesArray.sort().join("");
+  } else {
+    return false;
+  }
 };
