0afa9660065d1b11695bd2b4fcb710ba3dac2cae
Tim Sorokin <timsorokin352@gmail.com>
2020-10-12 16:18:03 +0300

    feat: complete what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  try {
+    date.getTime();
+    let month = date.getMonth();
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return "winter";
+        break;
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+        break;
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+        break;
+      case 8:
+      case 9:
+      case 10:
+        return "autumn";
+        break;
+    }
+  } catch {
+    throw new Error("Something went wrong!");
+  }
+}
