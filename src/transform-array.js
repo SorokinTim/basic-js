6e5d35351ead2df28c5da0322d28c763280789e2
Tim Sorokin <timsorokin352@gmail.com>
2020-10-14 13:32:48 +0300

    fix: fix error in transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,28 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  if (!Array.isArray(arr)) {
-    throw new Error("It isn\'t array!");
-  }
-
-  let newArr = [];
-
-  for (let i = 0; i < arr.length; i++) {
+module.exports = for (let i = 0; i < arr.length; i++) {
     switch (arr[i]) {
       case "--discard-next":
-        if(i == arr.length - 1) continue;
-        i += 2;
+        if(i == arr.length - 1 || arr[i+2] == "--discard-prev") continue;
+        i++;
         break;
       case "--discard-prev":
         if(i == 0) continue;
-        newArr.splice(i - 1, 1);
+        newArr.pop();
         break;
       case "--double-next":
         if(i == arr.length - 1) continue;
         newArr.push(arr[i + 1]);
         break;
       case "--double-prev":
-        if(i == 0) continue;
+        if(i == 0 || arr[i-2] == "--discard-next") continue;
         newArr.push(arr[i - 1]);
         break;
       default:
